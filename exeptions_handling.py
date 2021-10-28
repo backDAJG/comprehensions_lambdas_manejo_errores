1e08c03700a01f136c0f69017852831465329e54
Alejandro Jerez Colfenix GPS <jerez1.2.3.102107@gmail.com>
2021-10-28 14:46:38 -0500

    exeptions handling

---
+++ exeptions_handling.py
@@ -0,0 +1,17 @@
+def divisors(num):
+    if num < 0:
+        raise ValueError("No se se admiten numeros negativos")
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+
+    return divisors
+
+def run():
+    try:
+        num = input('Ingresa un numero: ')
+        print(divisors(num))
+        print("Termino mi programa")
+    except ValueError as ve:
+        print(ve)
+
+if __name__ == '__main__':
+    run()

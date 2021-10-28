e9bd980cb3c4e0f038b59f74077985740f46d1ef
Alejandro Jerez Colfenix GPS <jerez1.2.3.102107@gmail.com>
2021-10-28 14:27:45 -0500

    debugging

---
+++ debugging.py
@@ -0,0 +1,12 @@
+def divisors(num):
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+
+    return divisors
+
+def run():
+    num = int(input('Ingresa un numero: '))
+    print(divisors(num))
+    print("Termino mi programa")
+
+if __name__ == '__main__':
+    run()
